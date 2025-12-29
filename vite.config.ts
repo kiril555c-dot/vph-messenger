c25899a681420567eb7bc040ec721a9bb1c51bb2
kiri555c-dot <kiril555c@gmail.com>
2025-12-29 20:43:56 +0300

    add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vph-messenger/', // Добавлено для корректной работы на GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
