fa87ecfc7b83b2ffe13b0fddbec8e2a3dbd17454
kiri555c-dot <kiril555c@gmail.com>
2025-12-29 21:38:24 +0300

    fix: add prisma generate to build script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-httpServer.listen(PORT, () => {
+// Добавили '0.0.0.0' — это говорит серверу принимать запросы из интернета
+httpServer.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
